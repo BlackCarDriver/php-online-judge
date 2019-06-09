8b5cece8b74cc17f9c3a452e016d4b76b83ce159
BlackCarDriver <13169346002@163.com>
2019-06-09 19:00:04 +0800

    clear up 2

--- main.go
+++ main.go
@@ -1,41 +1,9 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"os/exec"
+import(
+	"./PhpOJ"
 )
 
-func main() {
-	// os.Chdir("../shell")
-	params := make([]string, 2)
-	params[0] = "-c"
-	params[1] = `
-	PHP=$(pwd)
-	docker run --name php \
-		--rm \
-		-i \
-		-v $PHP/code:/code \
-		php \
-		php /code/index.php`
-	// params[1] = "php.sh"
-	_ = execCommand("bash", params)
-}
-
-func execCommand(commandName string, params []string) bool {
-	cmd := exec.Command(commandName, params...)
-
-	//显示运行的命令
-	fmt.Println(cmd.Args)
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		return false
-	}
-	fmt.Println(out.String())
-	return true
+func main(){
+	oj.Run()
 }
